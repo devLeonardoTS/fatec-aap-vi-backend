250679cf6a9517c8ba4d49a29bad0b486e43ed90
Thiago Jeronimo <jeronimothiago96@gmail.com>
2025-03-15 01:29:30 -0300

    V 1.0.1 - CRUD

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 Route::get('/', function () {
   return response()->json('Hello World!');
 });
-Route::get('/messages',[MessageController::class,'index'])->name('messages.index');
-
-Route::get('/messages/create',[MessageController::class,'create']) ->name('messages.create');
-
-Route::post('/messages',[MessageController::class,'store']) ->name('messages.store');
-
-Route::get('/messages/{message}',[MessageController::class,'show']) ->name('messages.show');
-
-Route::get('/messages/{message}/edit',[MessageController::class,'edit']) ->name('messages.edit');
-
-Route::put('/messages/{message}',[MessageController::class,'update']) ->name('messages.update');
-
-Route::delete('/messages/{message}',[MessageController::class,'destroy']) ->name('messages.destroy'); 
+Route::resource('messages', MessageController::class);
 
 // Handles "auth:sanctum" redirect when user is not authenticated.
 Route::get('/login', function () {
